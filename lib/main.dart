c7347ddec63f4dc06c97a38bad2aab9976fcd343
kcm2112007 <sarkaricareerhubofficial@gmail.com>
2026-09-19 21:31:57 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,4 @@ import 'app.dart';
 
 void main() {
   runApp(const ImageGridMakerApp());
-}              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
 }
