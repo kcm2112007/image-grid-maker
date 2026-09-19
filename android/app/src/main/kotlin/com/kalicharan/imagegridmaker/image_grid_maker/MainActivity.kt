a835c37defd6d8c09f4c8b688336761a47496605
kcm2112007 <sarkaricareerhubofficial@gmail.com>
2026-09-19 15:06:38 +0000

    Scaffold Flutter project

---
+++ android/app/src/main/kotlin/com/kalicharan/imagegridmaker/image_grid_maker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.kalicharan.imagegridmaker.image_grid_maker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/kcm2112007/image_grid_maker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.kcm2112007.image_grid_maker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
