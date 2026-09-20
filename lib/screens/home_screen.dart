c7eb2b4460bb10f488b5b6df2f468b3ba2852ace
kcm2112007 <sarkaricareerhubofficial@gmail.com>
2026-09-20 18:23:16 +0530

    Fix import order in home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
-import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter/material.dart'; 
 import '../app.dart';
 import '../models/canvas_ratio.dart';
 import '../models/grid_layout.dart';
