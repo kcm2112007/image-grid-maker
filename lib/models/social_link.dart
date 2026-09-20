c7a9611d6ca582a2db634f5030739cf2987537db
kcm2112007 <sarkaricareerhubofficial@gmail.com>
2026-09-20 17:56:31 +0530

    Add SocialLink class and social links list
    Defines a SocialLink class and a list of social links for the Home screen footer.

---
+++ lib/models/social_link.dart
@@ -0,0 +1,37 @@
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+/// A single social/community link shown on the Home screen footer.
+class SocialLink {
+  final String label;
+  final String url;
+  final IconData icon;
+
+  const SocialLink({
+    required this.label,
+    required this.url,
+    required this.icon,
+  });
+}
+
+const List<SocialLink> kSocialLinks = [
+  SocialLink(
+    label: 'WhatsApp Channel',
+    url: 'https://whatsapp.com/channel/0029VbDU9dmEQIameOnw2R47',
+    icon: FontAwesomeIcons.whatsapp,
+  ),
+  SocialLink(
+    label: 'Instagram',
+    url: 'https://www.instagram.com/goodx_official?stkn=MjViOTB0M3o5OHN6',
+    icon: FontAwesomeIcons.instagram,
+  ),
+  SocialLink(
+    label: 'Facebook',
+    url: 'https://www.facebook.com/share/1Ew1TbBSwZ/',
+    icon: FontAwesomeIcons.facebook,
+  ),
+  SocialLink(
+    label: 'Threads',
+    url: 'https://www.threads.com/@goodx_official',
+    icon: FontAwesomeIcons.at, // Threads has no dedicated icon in this package yet
+  ),
+];
