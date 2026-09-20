f58126ad1152c65b0eaa3b2a8b7d73c4c5d3b55d
kcm2112007 <sarkaricareerhubofficial@gmail.com>
2026-09-20 18:21:12 +0530

    Update social link icons to Material icons
    Replaced FontAwesome icons with Material icons for social links.

--- lib/models/social_link.dart
+++ lib/models/social_link.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/widgets.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter/material.dart';
 
 /// A single social/community link shown on the Home screen footer.
 class SocialLink {
@@ -18,16 +17,16 @@ const List<SocialLink> kSocialLinks = [
   SocialLink(
     label: 'WhatsApp Channel',
     url: 'https://whatsapp.com/channel/0029VbDU9dmEQIameOnw2R47',
-    icon: FontAwesomeIcons.whatsapp,
+    icon: Icons.chat_outlined,
   ),
   SocialLink(
     label: 'Instagram',
     url: 'https://www.instagram.com/goodx_official?stkn=MjViOTB0M3o5OHN6',
-    icon: FontAwesomeIcons.instagram,
+    icon: Icons.camera_alt_outlined,
   ),
   SocialLink(
     label: 'Facebook',
     url: 'https://www.facebook.com/share/1Ew1TbBSwZ/',
-    icon: FontAwesomeIcons.facebook,
+    icon: Icons.public_outlined,
   ),
 ];
